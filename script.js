4ef0b74cc838ba5d00fff425a766ee87423b30c2
sushi057 <champsuvash@gmail.com>
2022-08-08 20:50:14 +0545

    add playRound fucntion

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 function computerChoice(){
-    hands = ['Rock', 'Paper', 'Scissor'];
+    hands = ['rock', 'paper', 'scissor'];
     computerHand = Math.floor(Math.random() * hands.length);
-    console.log(computerHand);
+    return hands[computerHand];
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = prompt("Choose Rock, Paper or Scissor!");
+    computerSelection = computerChoice();
+
+    var player = playerSelection.toUpperCase();
+    var computer = computerSelection.toUpperCase();
+
+    if(player == "ROCK"){
+        if (computer == "PAPER"){
+            return("You Lose! Paper beats Rock.");
+        }
+        else if(computer == "SCISSOR"){
+            return("You Win!. Scissor beats Rock.");
+        }
+    }
+
+    if (player == "PAPER"){
+        if(computer == "ROCK"){
+            return("You win!. Paper beats Rock.");
+        }
+        else if(computer == "SCISSOR"){
+            return("You lose! Scissor beats Paper.");
+        }
+    }
+
+    if (player == "SCISSOR"){
+        if(computer == "PAPER"){
+            return("You win!. Scissor beats Paper.");
+        }
+        else if(computer == "ROCK"){
+            return("You lose! Rock beats Scissor.");
+        }
+    }
 }
